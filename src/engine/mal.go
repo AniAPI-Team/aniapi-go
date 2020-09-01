829a736b14b4f2753303f4b4d361e55a20d37ba9
Dazorn96 <marcodz554@gmail.com>
2020-09-01 13:04:55 +0200

    Updated MyAnimeList anime title CSS selection

--- src/engine/mal.go
+++ src/engine/mal.go
@@ -106,7 +106,7 @@ func (m *MALSearch) scrapeElement(uri string) *models.Anime {
 
 	col := doc.Find("#content table tbody tr td").Eq(0).Find("div")
 
-	title, _ := doc.Find("#contentWrapper .h1-title span").Eq(0).Html()
+	title, _ := doc.Find("#contentWrapper .h1-title .name .title-name").Html()
 	anime.MainTitle = strings.Split(title, "<br/>")[0]
 	anime.Picture, _ = col.Find("div:nth-child(1) a img").Attr("data-src")
 
